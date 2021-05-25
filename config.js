d5b83129f9a3ad0e25817d859275edf06ef89ea1
Gauthamraj Elango <gauthamrajelango@gmail.com>
2021-05-26 00:24:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const config = {
   description:
     "List of all covid facilties in Karnataka",
   sideBarInfo: ["CVC Name", "Address", "District"],
-  popupInfo: ["CVC Name", "Address"],
+  popupInfo: ["CVC Name"],
   filters: [
     {
       type: "dropdown",
